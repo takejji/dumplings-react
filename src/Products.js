aff96de00d0bcb40299576099c75002259568616
Vladislav <takejji@gmail.com>
2024-06-10 02:01:35 +0300

    init

--- src/Products.js
+++ src/Products.js
@@ -18,17 +18,17 @@ const Products = ({ addToCart }) => {
   };
 
   const handleAddToCart = () => {
-    addToCart({ name: 'Пельмени', quantity: quantity, flavor: flavor, price: 80 }); // Добавляем цену
+    addToCart({ name: 'Пельмени', quantity: quantity, flavor: flavor, price: 80 }); 
   };
 
   return (
     <div>
       <h2>Товары</h2>
       <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={hinnn} /> {/* Добавляем изображение товара */}
+        <Card.Img variant="top" src={hinnn} /> 
         <Card.Body>
           <Card.Title>Пельмени</Card.Title>
-          <Card.Text>Ебейщие пельемени. 80 Руб.</Card.Text> {/* Добавляем описание и цену */}
+          <Card.Text>Ебейщие пельемени. 80 Руб.</Card.Text>
           <Form>
             <Form.Group as={Row} className="mb-3">
               <Form.Label column sm="4">Количество:</Form.Label>
@@ -52,7 +52,94 @@ const Products = ({ addToCart }) => {
           <Button onClick={handleAddToCart}>Добавить в корзину</Button>
         </Card.Body>
       </Card>
-      {/* Аналогично добавляем карточки для остальных товаров */}
+      {/* добавляем карточки для остальных товаров */}
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={hinnn} /> 
+        <Card.Body>
+          <Card.Title>Хинкали</Card.Title>
+          <Card.Text>Ебейщие хинкали. 80 Руб.</Card.Text>
+          <Form>
+            <Form.Group as={Row} className="mb-3">
+              <Form.Label column sm="4">Количество:</Form.Label>
+              <Col sm="8">
+                <Form.Control type="number" value={quantity} onChange={handleQuantityChange} />
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row} className="mb-3">
+              <Form.Label column sm="4">Вкус:</Form.Label>
+              <Col sm="8">
+                <Form.Select value={flavor} onChange={handleFlavorChange}>
+                  <option value="Говядина">Говядина</option>
+                  <option value="Свинина">Свинина</option>
+                  <option value="Курица">Лисички</option>
+                </Form.Select>
+              </Col>
+            </Form.Group>
+          </Form>
+          <Link to="/blog">Просмотреть заказ</Link> 
+          <hr/>
+          <Button onClick={handleAddToCart}>Добавить в корзину</Button>
+        </Card.Body>
+      </Card>
+      {/* добавляем карточки для остальных товаров */}
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={hinnn} /> 
+        <Card.Body>
+          <Card.Title>Гедза</Card.Title>
+          <Card.Text>Ебейщие Гедза. 80 Руб.</Card.Text>
+          <Form>
+            <Form.Group as={Row} className="mb-3">
+              <Form.Label column sm="4">Количество:</Form.Label>
+              <Col sm="8">
+                <Form.Control type="number" value={quantity} onChange={handleQuantityChange} />
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row} className="mb-3">
+              <Form.Label column sm="4">Вкус:</Form.Label>
+              <Col sm="8">
+                <Form.Select value={flavor} onChange={handleFlavorChange}>
+                  <option value="Говядина">Говядина</option>
+                  <option value="Свинина">Свинина</option>
+                  <option value="Курица">Курица</option>
+                </Form.Select>
+              </Col>
+            </Form.Group>
+          </Form>
+          <Link to="/blog">Просмотреть заказ</Link> 
+          <hr/>
+          <Button onClick={handleAddToCart}>Добавить в корзину</Button>
+        </Card.Body>
+      </Card>
+      {/* добавляем карточки для остальных товаров */}
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={hinnn} /> 
+        <Card.Body>
+          <Card.Title>Акиця Пельмени + Пиво</Card.Title>
+          <Card.Text>Пиво, кстати, тоже ебейщее. 80 Руб.</Card.Text>
+          <Form>
+            <Form.Group as={Row} className="mb-3">
+              <Form.Label column sm="4">Количество:</Form.Label>
+              <Col sm="8">
+                <Form.Control type="number" value={quantity} onChange={handleQuantityChange} />
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row} className="mb-3">
+              <Form.Label column sm="4">Вкус:</Form.Label>
+              <Col sm="8">
+                <Form.Select value={flavor} onChange={handleFlavorChange}>
+                  <option value="Говядина">Говядина + балтика 9</option>
+                  <option value="Свинина">Свинина + Жигулевское</option>
+                  <option value="Курица">Курица + Исса</option>
+                </Form.Select>
+              </Col>
+            </Form.Group>
+          </Form>
+          <Link to="/blog">Просмотреть заказ</Link> 
+          <hr/>
+          <Button onClick={handleAddToCart}>Добавить в корзину</Button>
+        </Card.Body>
+      </Card>
+      {/* добавляем карточки для остальных товаров */}
     </div>
   );
 };
